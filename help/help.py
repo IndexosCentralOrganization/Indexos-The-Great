7340d781ecfe9189e5662681b95ce43e197da4f7
gnouf1 <jeuxforum26@gmail.com>
2020-06-16 12:28:51 +0200

    ajout d toptag dans le help

--- help/help.py
+++ help/help.py
@@ -14,7 +14,7 @@ class Helpme(commands.Cog):
     @commands.command(pass_context=True)
     async def help(self, ctx, nameElem = None):
         """Affiche ce message !"""
-        await ctx.channel.send("**Commandes disponibles*** :\n\n **Ladd :** `Ladd LIEN optTAG1 OPTtag2 OPTtag3`\n Permet d'ajouter un lien avec 0, 1, 2 ou 3 tags.\n\n**Ldel :** `Ldel LIEN `\n Permet de supprimer un lien **SI ON EST L'AUTEUR DE SON AJOUT**.\n\n**Lsearch :** `Lsearch ITEM`\n Permet de rechercher un lien dont soit un des tags est ITEM ou dont le salon d'origine a pour nom ITEM.")
+        await ctx.channel.send("**Commandes disponibles*** :\n\n **Ladd :** `Ladd LIEN optTAG1 OPTtag2 OPTtag3`\n Permet d'ajouter un lien avec 0, 1, 2 ou 3 tags.\n\n**Ldel :** `Ldel LIEN `\n Permet de supprimer un lien **SI ON EST L'AUTEUR DE SON AJOUT**.\n\n**Lsearch :** `Lsearch ITEM`\n Permet de rechercher un lien dont soit un des tags est ITEM ou dont le salon d'origine a pour nom ITEM.\n\n**toptag :** `toptag optx`\n Affiche les x tags les plus utilisés, 10 par défaut.")
 
 
 def setup(bot):
