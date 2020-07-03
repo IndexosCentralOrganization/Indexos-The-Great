e02a5fac728b8a9fe1a5d0545e7033bad6dccbc8
gnouf1 <jeuxforum26@gmail.com>
2020-07-04 01:35:50 +0200

    ajout des backups

--- core.py
+++ core.py
@@ -2,10 +2,17 @@ import discord
 import DB.manageDB as mdb
 from discord.ext import commands
 from discord.ext.commands import Bot
+import apscheduler as sc
+from os import remove as rm
+import DB.manageDB as mdb
+import datetime as dt
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 # initialisation des variables.
 DEFAUT_PREFIX = "!"
 
+date = dt.datetime.now()
+DATE = "{}-{}-{}".format(str(date.day), str(date.month), str(date.year))
 VERSION = open("core/version.txt").read().replace("\n", "")
 TOKEN = open("token/token.txt", "r").read().replace("\n", "")
 PREFIX = open("core/prefix.txt", "r").read().replace("\n", "")
@@ -14,6 +21,16 @@ NONE = open("help/cogs.txt", "w")
 NONE = open("help/help.txt", "w")
 
 client.remove_command("help")
+
+async def backup():
+    channel = client.get_channel(589942497678196764)  # ID du chan "backup"
+    await channel.send("Backup du {}".format(DATE))
+    fp = mdb.dumpAllDB()
+    fileD = discord.File(fp, "backup{}.sql".format(DATE))
+    await channel.send(file=fileD)
+    rm(fp)
+    await channel.send("**=============**")
+
 # Au demarrage du Bot.
 @client.event
 async def on_ready():
@@ -25,6 +42,10 @@ async def on_ready():
         print("DB Valide")
     else:
         print("DB invalide")
+
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(backup, 'interval', hour=24)
+    scheduler.start()
     print("Indexos is ready for index stuff !")
 
 client.load_extension('commands')
