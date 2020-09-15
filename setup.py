993d2c5d1bd35d663c8b33e0a46c776792f0c753
gnouf1 <jeuxforum26@gmail.com>
2020-09-15 23:50:46 +0200

    ménage + requirement + setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='indexos-the-great',
+    install_requires=[
+        'validators==0.15.0',
+        'wikipedia==1.4.0',
+        'discord.py==1.4.1',
+        'webpreview==1.6.0',
+        'APScheduler==3.6.3',
+        'discord==1.0.1',
+        'pycryptodome==3.9.8',
+    ],
+    py_modules=[
+        'core',
+    ],
+)
