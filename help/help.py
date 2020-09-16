b48b710cc8fb90f2e464533306244f892a8ef0d9
gnouf1 <jeuxforum26@gmail.com>
2020-09-16 22:07:31 +0200

    Relooking et ajout

--- help/help.py
+++ help/help.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from discord.ext.commands import bot
+from discord import Color
 
 
 class Helpme(commands.Cog):
@@ -14,8 +14,9 @@ class Helpme(commands.Cog):
         """Affiche ce message !"""
         file = open("help/help_command.txt", 'r')
         content = file.read()
-        print(content)
-        await ctx.channel.send(content)
+        msg = discord.Embed(title="Commandes disponible", color=Color.red(), url="https://github.com/gnouf1/Indexos-The-Great/blob/master/help/help_command.txt", description=content)
+
+        await ctx.channel.send(embed=msg)
         # await ctx.channel.send("**Commandes disponibles*** :\n\n **Ladd :** `Ladd LIEN optTAG1 OPTtag2 OPTtag3`\n Permet d'ajouter un lien avec 0, 1, 2 ou 3 tags.\n\n**Ldel :** `Ldel LIEN `\n Permet de supprimer un lien **SI ON EST L'AUTEUR DE SON AJOUT**.\n\n**Lsearch :** `Lsearch ITEM`\n Permet de rechercher un lien dont soit un des tags est ITEM ou dont le salon d'origine a pour nom ITEM.\n\n**toptag :** `toptag optx`\n Affiche les x tags les plus utilisés, 10 par défaut.")
 
 
